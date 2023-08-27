284a71517e96a17388b23e712190ae56d67e00f4
Ravihara De Silva <117923473+Ravihara2000@users.noreply.github.com>
2023-08-27 11:07:18 +0530

    sql query to insert data

--- Frontend/emp_data.php
+++ Frontend/emp_data.php
@@ -11,6 +11,16 @@ if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
 
+if(isset($_POST['id']) && isset($_POST['name']) && isset($_POST['gender']) && isset($_POST['location']) && isset($_POST['age']) && isset($_POST['bgroup'])) {
+    $id = (int)$_POST['id'];
+    $name = mysqli_real_escape_string($conn, $_POST['name']);
+    $gender = mysqli_real_escape_string($conn, $_POST['gender']);
+    $location = mysqli_real_escape_string($conn, $_POST['location']);
+    $age = (int)$_POST['age'];
+    $bgroup = mysqli_real_escape_string($conn, $_POST['bgroup']);
+
+    $sql = "INSERT INTO employeerecods.employee(EmpID, EmpName, EmpGender, EmpLocation, EmpAge, EmpBloodGroup) 
+            VALUES('$id', '$name', '$gender', '$location', '$age', '$bgroup')";
 
 
 mysqli_close($conn);
