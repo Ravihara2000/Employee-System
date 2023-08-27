92f721da9a2d6e8b2f56663516834cc029d99697
Ravihara De Silva <117923473+Ravihara2000@users.noreply.github.com>
2023-08-27 11:13:47 +0530

    added sql query

--- Frontend/view_data.php
+++ Frontend/view_data.php
@@ -20,3 +20,6 @@ $conn = new mysqli($servername, $username, $password, $dbname);
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
+
+$sql = "SELECT * FROM employeerecods.employee";
+$result = mysqli_query($conn, $sql);
