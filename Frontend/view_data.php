115d267b5c4bbfd8b02552c048929b4681a4d1df
Ravihara De Silva <117923473+Ravihara2000@users.noreply.github.com>
2023-08-27 11:14:37 +0530

    create the table to display data

--- Frontend/view_data.php
+++ Frontend/view_data.php
@@ -23,3 +23,31 @@ if ($conn->connect_error) {
 
 $sql = "SELECT * FROM employeerecods.employee";
 $result = mysqli_query($conn, $sql);
+
+echo "<table id='recods'>";
+echo "<tr>";
+
+echo "<th>Employee ID</th>";
+echo "<th>Employee Name</th>";
+echo "<th>Employee Gender</th>";
+echo "<th>Employee Location</th>";
+echo "<th>Employee Age</th>";
+echo "<th>Employee Blood Group</th>";
+
+echo "</tr>";
+
+if (mysqli_num_rows($result) > 0) {
+    while ($row = mysqli_fetch_assoc($result)) {
+        echo "<tr>";
+        echo "<td>", $row['EmpID'], "</td>";
+        echo "<td>", $row['EmpName'], "</td>";
+        echo "<td>", $row['EmpGender'], "</td>";
+        echo "<td>", $row['EmpLocation'], "</td>";
+        echo "<td>", $row['EmpAge'], "</td>";
+        echo "<td>", $row['EmpBloodGroup'], "</td>";
+        echo "</tr>";
+    }
+    echo "</table>";
+} else {
+    echo "<tr><td colspan='6'>Empty Table</td></tr></table>";
+}
